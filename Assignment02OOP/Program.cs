2e00755dde4aca1d79d3ec57e133ede2c34e5d89
martin reda <martinreda999@outlook.com>
2025-07-19 07:26:47 +0300

    Add project files.

---
+++ Assignment02OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment02OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
