a6be8c9418ec737b204df3f4088c81757b9e1955
martin reda <martinreda999@outlook.com>
2025-07-19 08:43:46 +0300

    04 Encapsulation

---
+++ Demo02oop/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo02oop
+{
+    internal struct Employee
+    {
+        #region Attrubites
+        public int Id;
+        public string name;
+        public int age;
+        #endregion
+
+        #region CTOR
+        //3ndi 3 ways a3ml behm al CTOR 
+        /*1- public employee() => manually
+         *2-CTOR + Tab => empty CTOR
+         *3-From attrbites all data */
+        
+        public Employee(int id, string name, int age)
+        {
+            this.Id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        #endregion
+
+        //applay encapsolation 
+        /* 01 all attrabutes privte .
+         * 02 use setter getter methods 
+         * 03 use properites*/
+
+        #region Apply encapsulation using setter Getter methods
+        // Getter => get name 
+        public string GetName ()
+        {
+            return name;
+        }
+
+        //Setters
+        public void SetName(string Name)
+        {
+            Name = name;
+        }
+
+        #endregion
+        #region Method 
+
+        public override string ToString()
+        {
+            return $"id : {Id} \n Name : {name} \n Age : {age}";
+        }
+        #endregion
+
+    }
+}
--- Demo02oop/Program.cs
+++ Demo02oop/Program.cs
@@ -13,11 +13,11 @@
              */
 
             //Calling  i must have CTOR on strucrt
-            Point P1;
+            //Point P1;
 
-            P1 = new Point(1,2); // New just for ctor selection not referance 
-                          //assign x = 1 y = 2
-            Console.WriteLine(P1);
+            //P1 = new Point(1,2); // New just for ctor selection not referance 
+            //              //assign x = 1 y = 2
+            //Console.WriteLine(P1);
 
             /*struct at memory
              * stack
@@ -28,10 +28,36 @@
 
             #region OOP Defination
             /*
-             * every thing is object
+             * every thing is Focus on >= object
+             * C# pure OOP
              * oop aims to structure software in a way thats model real entites using oop concepts 
              * zy al 3rabya bttgmm3 wa7da wa7da 3la4n nkwanhaa 
+             * 1- encapsolathion
+             * 2-inhertance
+             * 3-polymorphisn
+             * 4-abstractin
              */
+            #endregion
+
+            #region Encapsulation
+            Employee emp01 = new Employee(1, "martin",1000); // new for ctor selection
+            //Console.WriteLine( emp01);
+
+            //emp01.Id = 22;
+            //emp01.name = "R";
+            //Console.WriteLine(emp01); //struct not protactive
+
+
+            // using getter
+            Console.WriteLine(emp01.GetName());
+
+            //using setter
+            emp01.SetName("mm");
+            Console.WriteLine(emp01.GetName());
+
+
+
+
             #endregion
 
         }
