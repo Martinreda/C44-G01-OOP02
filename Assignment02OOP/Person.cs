4bcce474ba8c74492f18d015b6d2d833d09c4370
martin reda <martinreda999@outlook.com>
2025-07-20 20:04:01 +0300

    Q01

---
+++ Assignment02OOP/Person.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02OOP
+{
+    internal struct  Person
+    {
+        #region Attributes
+        private string[] names;
+        private int[] ages;
+        private int size;
+        #endregion
+
+        #region Constructor
+        public Person (int size)
+        {
+            this.size = size;
+            names = new string[size];
+            ages = new int[size];
+        }
+        #endregion
+
+        #region Properties
+        public int Size
+        {
+            get { return size; }
+        }
+        #endregion
+
+        #region Methods
+        public void AddPerson(int index, string name, int age)
+        {
+            if (index >= 0 && index < size)
+            {
+                names[index] = name;
+                ages[index] = age;
+            }
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine("List of People:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Person {i + 1}: Name = {names[i]}, Age = {ages[i]}");
+            }
+        }
+        #endregion
+
+        #region Indexer
+        public int this[string name]
+        {
+            get
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (names[i] == name)
+                        return ages[i];
+                }
+                return -1;
+            }
+            set
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (names[i] == name)
+                    {
+                        ages[i] = value;
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
+
+
+
--- Assignment02OOP/Program.cs
+++ Assignment02OOP/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");يي
+            #region Q01
+            /*1.	Define a struct "Person" with properties "Name" and "Age". 
+             *      Create an array of three "Person" objects and populate it with data.
+             *       Then, write a C# program to display the details of all the persons in the array.*/
+
+
+            Person people = new Person(3);
+
+            people.AddPerson(0, "Martin", 26);
+            people.AddPerson(1, "hossam", 30);
+            people.AddPerson(2, "Youssef", 22);
+
+            people.ShowAll();
+
+            Console.WriteLine($"\nhossam's Age Before: {people["hossam"]}");
+            people["hossam"] = 35;
+            Console.WriteLine($"hossam's Age After: {people["hossam"]}");
+
+            #endregion
         }
     }
 }
